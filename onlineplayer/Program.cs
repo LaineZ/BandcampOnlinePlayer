d7bc5302b473f04f5a1bc43619909e36e9af465c
140bpmdubstep <laineprikol@gmail.com>
2020-01-01 15:50:04 +0400

    Added config creation

--- onlineplayer/Program.cs
+++ onlineplayer/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.IO;
+using System.Xml;
 
 namespace onlineplayer
 {
@@ -13,6 +15,36 @@ namespace onlineplayer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            if (!File.Exists("settings.xml"))
+            {
+                XmlWriter xmlWriter = XmlWriter.Create("settings.xml");
+
+                xmlWriter.WriteStartDocument();
+                xmlWriter.WriteStartElement("settings");
+
+                xmlWriter.WriteStartElement("setting");
+                xmlWriter.WriteAttributeString("albumViewType", "Tile");
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteStartElement("setting");
+                xmlWriter.WriteAttributeString("albumViewSize", "64");
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteStartElement("setting");
+                xmlWriter.WriteAttributeString("saveArtworks", "True");
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteStartElement("setting");
+                xmlWriter.WriteAttributeString("loadPages", "100");
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteStartElement("setting");
+                xmlWriter.WriteAttributeString("midiDevice", "0");
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteEndDocument();
+                xmlWriter.Close();
+            }
             Application.Run(new Form1());
         }
     }
